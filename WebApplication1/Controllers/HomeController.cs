cb3f802603228c6cdc3ef434a25640d7da390cf1
thien dao <thienpc147@gmail.com>
2020-05-13 20:04:40 +0700

    test

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace WebApplication1.Controllers
         {
             return View();
         }
+        public IActionResult Test()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
